ab49634598d12fc6c62d4aa8ac58758712699747
antoniosacchetti <antoniosacchetti@ua.pt>
2025-03-10 17:57:14 +0000

    ConverterTemperatura
    classe e metudos para converter temperatura

--- demo/src/main/java/com/UtilsLibrary/Conversor.java
+++ demo/src/main/java/com/UtilsLibrary/Conversor.java
@@ -1,5 +1,46 @@
 package com.UtilsLibrary;
 
+
 public class Conversor {
+    
+    public class ConverterTemperatura {
+        private double valor;
+        private String unidadeOrigem;
+        private String unidadeDestino;
+    
+        public ConverterTemperatura( double valor, String unidadeOrigem, String unidadeDestino) {
+        this.valor = valor;
+        this.unidadeOrigem = unidadeOrigem;
+        this.unidadeDestino = unidadeDestino;
+        }
+
+        public double converter(){
+            if (unidadeOrigem.equals(unidadeDestino)){
+                return valor;
+            }
+
+            double ValorEmCelsius;
+
+            switch (unidadeOrigem) {
+                case "FAHRENHEIT":
+                    ValorEmCelsius = (VALOR - 32) * 5/9;                    
+                    break;
+                case "KELVIN":
+                    ValorEmCelsius = valor - 273.15;
+                    break;
+                case "CELSIUS":
+                    ValorEmCelsius = valor;
+                    break;
+
+                default:
+                    throw new IllegalArgumentException("unidade de destino invalida: " + unidadeDestino);
+            }
+
+        }
+
+
+    }
+    
+
     
 }
